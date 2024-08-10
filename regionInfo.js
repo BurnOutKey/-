f8d79f0127c7c1d815ad40065d8f42cf72e95c8d
BurnOutKey <burn.out.wealthy@gmail.com>
2024-08-10 11:39:50 +0300

    Update regionInfo.js

---
+++ regionInfo.js
@@ -0,0 +1,37 @@
+module.exports = {
+                0: {
+                    name: "Local",
+                    latitude: 0,
+                    longitude: 0,
+                },
+                "us-east": {
+                    name: "Miami",
+                    latitude: 40.1393329,
+                    longitude: -75.8521818,
+                },
+                "us-west": {
+                    name: "Silicon Valley",
+                    latitude: 47.6149942,
+                    longitude: -122.4759879,
+                },
+                gb: {
+                    name: "London",
+                    latitude: 51.5283063,
+                    longitude: -0.382486,
+                },
+                "eu-west": {
+                    name: "Frankfurt",
+                    latitude: 50.1211273,
+                    longitude: 8.496137,
+                },
+                au: {
+                    name: "Sydney",
+                    latitude: -33.8479715,
+                    longitude: 150.651084,
+                },
+                sg: {
+                    name: "Singapore",
+                    latitude: 1.3147268,
+                    longitude: 103.7065876,
+                },
+            };
